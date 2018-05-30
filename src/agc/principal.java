316520843646d8e064cbbc62732318b2d19284e7
guille <100348777@alumnos.uc3m.es>
2018-05-30 10:25:52 +0200

    result to json file

--- src/agc/principal.java
+++ src/agc/principal.java
@@ -1,5 +1,8 @@
 package agc;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 import agc.data.AccelerationByAxes;
 import agc.data.DistByAxes;
 import agc.data.FullTurnByAxes;
@@ -8,7 +11,7 @@ import agc.exceptions.AGCException;
 
 public class principal {
 
-	public static void main(String[] args) throws AGCException {
+	public static void main(String[] args) throws AGCException, IOException {
 		// TODO Auto-generated method stub
 		
 		AGCManager experimento = new AGCManager();
@@ -19,6 +22,11 @@ public class principal {
 		
 		System.out.println("velmedia: "+"\n"+result.getX_Axis().getspeedValue()+ "\n"+result.getY_Axis().getspeedValue()+ "\n"+result.getZ_Axis().getspeedValue());
 		System.out.println("varianza: "+"\n"+result.getX_Axis().getDesvValue()+ "\n"+result.getY_Axis().getDesvValue()+ "\n"+result.getZ_Axis().getDesvValue());
+		
+		try (FileWriter file = new FileWriter("result.json")) {
+			file.write(result.toString());
+			System.out.println("Successfully Copied JSON Object to File...");
+		}
 
 	}
 
